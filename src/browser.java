a6a857f98b5b4fa96db119ae43ca48bb16125638
mamtajuyal <mamtapurohit89@gmail.com>
2016-02-05 19:38:56 +0530

    browser file edited with comment

--- src/browser.java
+++ src/browser.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
+/*This is a Browser Class*/
 public class browser {
 
 	public static void main(String[] args)
